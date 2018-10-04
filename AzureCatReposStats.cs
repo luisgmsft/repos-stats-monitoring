a892257caa361381ebc93d5e7bde745594ed2dfb
Luis Gizirian <v-lugizi@microsoft.com>
2018-10-04 17:57:56 -0300

    Lists only those repos where the logged user is an admin

--- AzureCatReposStats.cs
+++ AzureCatReposStats.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -23,7 +24,7 @@ namespace repos_stats
         public static async Task<IActionResult> GetStats([HttpTrigger(AuthorizationLevel.Function, "get", Route = "stats")]HttpRequest req, ILogger log)
         {
             var repos = await _githubClient.ListRepositories();
-            repos.ForEach(async e => {
+            repos.Where(e => e.Permissions.Admin).ToList().ForEach(async e => {
                 try
                 {
                     var traffic = await _githubClient.ListTrafficViews(log, e.Owner.Login, e.Name);
