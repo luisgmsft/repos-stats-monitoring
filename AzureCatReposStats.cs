80e4b0d66af1ca539376c043fc9a9fe11ef40c79
Luis Gizirian <v-lugizi@microsoft.com>
2018-10-09 21:07:32 -0300

    timerTrigger (commented still, but emulated into httpTrigger. Writing everything to CosmosDB (SQL API)

--- AzureCatReposStats.cs
+++ AzureCatReposStats.cs
@@ -15,13 +15,72 @@ using repos_stats.Models;
 
 namespace repos_stats
 {
+    public class RepoStat
+    {
+        public RepoStat()
+        {
+            Created = DateTime.UtcNow;
+        }
+
+        public DateTime Created { get; private set; }
+        public string Repo { get; set; }
+        public int Subscribers { get; set; }
+        public int Stargazers { get; set; }
+        public int Issues { get; set; }
+        public int Views { get; set; }
+        public int UniqueViews { get; set; }
+        public int UniqueClones { get; set; }
+        public int PRs { get; set; }
+    }
+
     public static class AzureCatReposStats
     {
         // https://docs.microsoft.com/en-us/azure/azure-functions/manage-connections
         private static GitHubClient _githubClient = new GitHubClient();
 
+        //[FunctionName("cron-stats")]
+        // public static async Task CronStats(
+        //     [TimerTrigger("0 0 1 * * *")]TimerInfo myTimer
+        //     , [CosmosDB(
+        //         databaseName: "repos-stats-sql-database",
+        //         collectionName: "repos-stats-sql-collection",
+        //         ConnectionStringSetting = "CosmosDBConnection")]IAsyncCollector<RepoStat> statsOut
+        //     , ILogger log)
+        // {
+        //     var repos = await _githubClient.ListRepositories();
+        //     //repos.ForEach(async e => {
+        //     repos.Where(e => e.Permissions.Admin).ToList().ForEach(async e => {
+        //         try
+        //         {
+        //             var traffic = await _githubClient.ListTrafficViews(log, e.Owner.Login, e.Name);
+        //             var clones = await _githubClient.ListClones(log, e.Owner.Login, e.Name);
+        //             var prs = await _githubClient.ListPRs(log, e.Owner.Login, e.Name);
+        //             await statsOut.AddAsync(new RepoStat {
+        //                 Repo = e.FullName,
+        //                 Subscribers = e.SubscribersCount,
+        //                 Stargazers = e.StargazersCount,
+        //                 Issues = e.OpenIssuesCount,
+        //                 Views = traffic.Count,
+        //                 UniqueViews = traffic.Uniques,
+        //                 UniqueClones = clones.Uniques,
+        //                 PRs = prs.Count
+        //             });
+        //         }
+        //         catch (Exception ex)
+        //         {
+        //             log.LogError(ex.Message);
+        //         }
+        //     });
+        // }
+
         [FunctionName("get-stats")]
-        public static async Task<IActionResult> GetStats([HttpTrigger(AuthorizationLevel.Function, "get", Route = "stats")]HttpRequest req, ILogger log)
+        public static async Task<IActionResult> GetStats(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "stats")]HttpRequest req
+            , [CosmosDB(
+                databaseName: "repos-stats-sql-database",
+                collectionName: "repos-stats-sql-collection",
+                ConnectionStringSetting = "CosmosDBConnection")]IAsyncCollector<RepoStat> statsOut
+            , ILogger log)
         {
             var repos = await _githubClient.ListRepositories();
             //repos.ForEach(async e => {
@@ -31,7 +90,16 @@ namespace repos_stats
                     var traffic = await _githubClient.ListTrafficViews(log, e.Owner.Login, e.Name);
                     var clones = await _githubClient.ListClones(log, e.Owner.Login, e.Name);
                     var prs = await _githubClient.ListPRs(log, e.Owner.Login, e.Name);
-                    log.LogInformation($"Repo: {e.FullName}\nSubscribers: {e.SubscribersCount}\nStargazers: {e.StargazersCount}\nIssues: {e.OpenIssuesCount}\nViews: {traffic.Count}\nUnique Views: {traffic.Uniques}\nUnique Clones: {clones.Uniques}\nPRs: {prs.Count}");
+                    await statsOut.AddAsync(new RepoStat {
+                        Repo = e.FullName,
+                        Subscribers = e.SubscribersCount,
+                        Stargazers = e.StargazersCount,
+                        Issues = e.OpenIssuesCount,
+                        Views = traffic.Count,
+                        UniqueViews = traffic.Uniques,
+                        UniqueClones = clones.Uniques,
+                        PRs = prs.Count
+                    });
                 }
                 catch (Exception ex)
                 {
